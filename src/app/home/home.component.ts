bab424490129c63c56bd003ff2cfe00da8f401b6
VenkyGunna <venky.it232530@gmail.com>
2024-10-05 22:39:02 +0530

    s8 carousel added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(public router:Router,public titleService:Title){
+  constructor(public router:Router,public titleService:Title,public renderer:Renderer2){
   }
   ngOnInit() {
     this.titleService.setTitle('Netsim Inc Home');
   }
   gotoURL(url:any){
     this.router.navigate([url]);
+  }
+  prevSlide() {
+    const carousel = document.getElementById('carouselExample');
+    this.renderer.setProperty(carousel, 'carousel', 'prev');
+  }
+
+  nextSlide() {
+    const carousel = document.getElementById('carouselExample');
+    this.renderer.setProperty(carousel, 'carousel', 'next');
   }
 
 }
