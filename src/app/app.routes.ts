f3d5591553457889799b258d7dd2e0ede97d6438
VenkyGunna <venky.it232530@gmail.com>
2024-10-18 00:48:34 +0530

    ai cybersecurity integrated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,60 +14,86 @@ import { ContractLocalTalentComponent } from './contract-local-talent/contract-l
 import { OffshoreTalentComponent } from './offshore-talent/offshore-talent.component';
 import { PermanentPlacementComponent } from './permanent-placement/permanent-placement.component';
 import { VeteranTalentComponent } from './veteran-talent/veteran-talent.component';
+import { CyberSecurityHomeComponent } from './cyber-security-home/cyber-security-home.component';
+import { CyberSecurityAssessmentComponent } from './cyber-security-assessment/cyber-security-assessment.component';
+import { CyberSecurityTestingComponent } from './cyber-security-testing/cyber-security-testing.component';
+import { CyberSecuritySolutionsComponent } from './cyber-security-solutions/cyber-security-solutions.component';
+import { AiMethodologyComponent } from './ai-methodology/ai-methodology.component';
+import { AiHomeComponent } from './ai-home/ai-home.component';
+import { AiChallengeComponent } from './ai-challenge/ai-challenge.component';
+import { AiSolutionsComponent } from './ai-solutions/ai-solutions.component';
 
 export const routes: Routes = [
     {
-        path:'',
-        component:HomeComponent,
-        pathMatch:'full'
+        path:'', component:HomeComponent, pathMatch:'full'
     },
     {
-        path:'aboutus',
-        component:AboutusComponent,
+        path:'aboutus', component:AboutusComponent,
     },
     {
-        path:'contactus',
-        component:ContactusComponent,
+        path:'contactus', component:ContactusComponent,
     },
     {
-        path:'clients',
-        component:ClientsComponent,
+        path:'clients', component:ClientsComponent,
     },
     {
-        path:'managed-solutions',component:ManagedSolutionsComponent
+        path:'managed-solutions', component:ManagedSolutionsComponent
     },
     {
-        path:'app-solutions',component:AppSolutionsComponent
+        path:'app-solutions', component:AppSolutionsComponent
     },
     {
-        path:'mobile-solutions',component:DigitalMobileSolutionsComponent
+        path:'mobile-solutions', component:DigitalMobileSolutionsComponent
     },
     {
-        path:'data-intelligence',component:DataIntelligenceComponent
+        path:'data-intelligence', component:DataIntelligenceComponent
     },
     {
-        path:'cloud-platform',component:CloudPlatformComponent
+        path:'cloud-platform', component:CloudPlatformComponent
     },
     {
-        path:'project-business-agility',component:ProjectBusinessAgilityComponent
+        path:'project-business-agility', component:ProjectBusinessAgilityComponent
     },
     {
-        path:'source-procurement',component:SourceProcurementComponent
+        path:'source-procurement', component:SourceProcurementComponent
     },
     {
-        path:'contract-local-talent',component:ContractLocalTalentComponent
+        path:'contract-local-talent', component:ContractLocalTalentComponent
     },
     {
-        path:'offshore-talent',component:OffshoreTalentComponent
+        path:'offshore-talent', component:OffshoreTalentComponent
     },
     {
-        path:'permanent-placement',component:PermanentPlacementComponent
+        path:'permanent-placement', component:PermanentPlacementComponent
     },
     {
-        path:'veteran-talent',component:VeteranTalentComponent
+        path:'veteran-talent', component:VeteranTalentComponent
     },
     {
-        path:'**',
-        redirectTo:'/'
+        path:'cybersecurity-home', component:CyberSecurityHomeComponent
+    },
+    {
+        path:'cybersecurity-assessment', component:CyberSecurityAssessmentComponent
+    },
+    {
+        path:'cybersecurity-testing', component:CyberSecurityTestingComponent
+    },
+    {
+        path:'cybersecurity-solution', component:CyberSecuritySolutionsComponent
+    },
+    {
+        path:'ai-methodology', component:AiMethodologyComponent
+    },
+    {
+        path:'ai-home', component:AiHomeComponent
+    },
+    {
+        path:'ai-challenge', component:AiChallengeComponent
+    },
+    {
+        path:'ai-solutions', component:AiSolutionsComponent
+    },
+    {
+        path:'**', redirectTo:'/'
     }
 ];
--- src/app/cyber-security-home/cyber-security-home.component.ts
+++ src/app/cyber-security-home/cyber-security-home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @Component({
   selector: 'app-cyber-security-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,SharedModule],
   templateUrl: './cyber-security-home.component.html',
   styleUrl: './cyber-security-home.component.scss'
 })
